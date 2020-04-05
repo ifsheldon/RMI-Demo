4ef0b5f6a099fcf111124b76f2d5bfa61bd94f3a
Maple <maplevantablack@outlook.com>
2020-04-05 19:57:06 +0800

    implemented methods but not tested

--- src/main/java/myrmi/server/StubInvocationHandler.java
+++ src/main/java/myrmi/server/StubInvocationHandler.java
@@ -1,42 +1,60 @@
 package myrmi.server;
 
 import myrmi.exception.RemoteException;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import myrmi.registry.Registry;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.net.InetSocketAddress;
 import java.net.Socket;
 
-public class StubInvocationHandler implements InvocationHandler, Serializable {
+public class StubInvocationHandler implements InvocationHandler, Serializable
+{
     private String host;
     private int port;
     private int objectKey;
 
-    public StubInvocationHandler(String host, int port, int objectKey) {
+    public StubInvocationHandler(String host, int port, int objectKey)
+    {
         this.host = host;
         this.port = port;
         this.objectKey = objectKey;
         System.out.printf("Stub created to %s:%d, object key = %d\n", host, port, objectKey);
     }
 
-    public StubInvocationHandler(RemoteObjectRef ref) {
+    public StubInvocationHandler(RemoteObjectRef ref)
+    {
         this(ref.getHost(), ref.getPort(), ref.getObjectKey());
     }
 
     @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws RemoteException, IOException, ClassNotFoundException, Throwable {
+    public Object invoke(Object callingProxy, Method method, Object[] args) throws RemoteException, IOException, ClassNotFoundException, Throwable
+    {
         /*TODO: implement stub proxy invocation handler here
          *  You need to do:
          * 1. connect to remote skeleton, send method and arguments
          * 2. get result back and return to caller transparently
          * */
-        throw new NotImplementedException();
-
+        Socket socket = new Socket("localhost", Registry.REGISTRY_PORT);/////////////
+        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+        Message request = new Message(objectKey, method.getName(), args);
+        oos.writeObject(request);
+        ObjectInputStream oin = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+        Message reply = (Message) oin.readObject();
+        switch (reply.getStatus())
+        {
+            case None:
+                throw new RemoteException("None replied");
+            case ExceptionThrown:
+            case InvocationError:
+                throw (Throwable) reply.result;
+            case ServerSideError:
+                throw new RemoteException("ServerSideError");
+            case Success:
+                return reply.result;
+            default:
+                return null;
+        }
     }
 
 }
