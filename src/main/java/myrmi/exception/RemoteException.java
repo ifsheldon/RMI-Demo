72169ac70270abf7246b1d1ef5e06554520fadd3
Maple <maplevantablack@outlook.com>
2020-04-05 17:30:08 +0800

    modified RemoteException's constructors

--- src/main/java/myrmi/exception/RemoteException.java
+++ src/main/java/myrmi/exception/RemoteException.java
@@ -1,4 +1,23 @@
 package myrmi.exception;
 
-public class RemoteException extends java.io.IOException{
+public class RemoteException extends java.io.IOException
+{
+    public RemoteException()
+    {
+    }
+
+    public RemoteException(String message)
+    {
+        super(message);
+    }
+
+    public RemoteException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    public RemoteException(Throwable cause)
+    {
+        super(cause);
+    }
 }
